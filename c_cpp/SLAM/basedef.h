97468f048e1397751fa7d5181ba54d2104779ded
hristoskov <hristo.hristoskov@gmail.com>
2015-01-26 11:14:14 +0100

    Marked bool operator < of pts struct as const, previously rising error in compilation.

--- c_cpp/SLAM/basedef.h
+++ c_cpp/SLAM/basedef.h
@@ -32,18 +32,25 @@ class pts {
         double x;
         double y;
 
-    	bool operator < (const pts& rhs) {
-        	if (x < rhs.x) return true;
-        	if ((x == rhs.x)&&(y < rhs.y)) return true;
+    	bool operator < (const pts& rhs) const {
+    		bool bResult = false;
+        	if (this->x < rhs.x)
+        	{
+        		bResult =  true;
+        	}
+        	else if ( (this->x == rhs.x) && (this->y < rhs.y) )
+        	{
+        		bResult = true;
+        	}
 			//if (sqrt((x-rhs.x)*(x-rhs.x)+(y-rhs.y)*(y-rhs.y)) < maxDist) {
 				//maxDist = sqrt((x-rhs.x)*(x-rhs.x)+(y-rhs.y)*(y-rhs.y));
 				//return true;
 			//}
-        	return false;
+        	return bResult;
    		}
 
 		bool operator == (const pts& rhs) {
-			return ((rhs.x == x)&&(rhs.y == y));
+			return ((rhs.x == this->x)&&(rhs.y == this->y));
 		}
 
 };
@@ -60,7 +67,7 @@ class lns {
 		}
 
 		bool operator < (lns& rhs) {
-			return lineSize() < rhs.lineSize();
+			return (this->lineSize() < rhs.lineSize());
 		}
 };
 
